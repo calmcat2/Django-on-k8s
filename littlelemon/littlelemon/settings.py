9058ca2c0a5b112933fc850df7c5fd22e7313a7a
Maxine <hmx222@gmail.com>
2025-02-01 14:14:26 -0700

    workflow

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -17,7 +17,6 @@ DEBUG = os.getenv("DEBUG")
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS').split(',')
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
