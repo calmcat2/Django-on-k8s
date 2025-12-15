5f5ed0ed9bd32b5ba5f658ea96786b29ef630a02
calmcat2 <22280915+calmcat2@users.noreply.github.com>
2025-12-14 21:33:29 -0700

    fix metrics path

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('api-token-auth', obtain_auth_token),
     path('auth', include('djoser.urls')),
     path('auth', include('djoser.urls.authtoken')),
-    path('metrics/', views.metrics_view),
+    path('metrics', views.metrics_view),
  
 ]
